3ac8f3e36175a92f6a5e686a7ece7a6ed957a7a9
Daniel Bratulić <danielbratulic@yahoo.com>
2018-03-30 14:47:27 +0200

    Changed the way ShotchartRetriever was called and added call for PlayersRetriever

--- retrieval/api_retriever_factory.py
+++ retrieval/api_retriever_factory.py
@@ -1,14 +1,25 @@
 from .api_retriever import ApiRetriever
+from .shotchart_retriever import ShotchartRetriever
+from .players_retriever import PlayersRetriever
 
 
 class ApiRetrieverFactory():
 
-    def create_regular_retriever_for_player(self, player_id, season):
+    def create_regular_shotchart_retriever_for_player(self, player_id, season):
         """
         Method which creates classic retrieval class which fetches all shots and all stats in areas throughout
         one season. Simplest method of creation of ApiRetriever object.
         :param player_id: Id of a player that represents him in NBA database.
         :param season: Season from which the stats will be fetched.
-        :return: ApiRetriever ready for retrieving data.
+        :return: ShotchartRetriever ready for retrieving data.
         """
-        return ApiRetriever(player_id=player_id, season=season)
+        return ShotchartRetriever(player_id=player_id, season=season)
+
+
+    def create_players_retriever_for_current_seasonn(self):
+        """
+        Creates the PlayersRetriever class which can retriever all players or only one of them based on further
+        calls by user.
+        :return: PlayersRetriever instance.
+        """
+        return PlayersRetriever()
